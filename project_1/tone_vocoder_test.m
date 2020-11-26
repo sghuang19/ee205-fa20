9c3455e7844b92c6c028953f8f16ef7c51448410
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-27 00:20:03 +0800

    Update tone_vocoder_test.m

--- project_1/tone_vocoder_test.m
+++ project_1/tone_vocoder_test.m
@@ -1,1 +1,27 @@
+clear
+close all
 [x, Fs] = audioread('C_01_01.wav');
+
+% Task - 1
+Fc = 50;
+N1_50 = tone_vocoder(x, Fs, 1, Fc);
+audiowrite('N1_50.wav', N1_50, Fs);
+subplot(4, 1, 1)
+plot(N1_50)
+
+N2_50 = tone_vocoder(x, Fs, 2, Fc);
+audiowrite('N2_50.wav', N2_50, Fs);
+subplot(4, 1, 2)
+plot(N2_50)
+
+N4_50 = tone_vocoder(x, Fs, 4, Fc);
+audiowrite('N4_50.wav', N4_50, Fs);
+subplot(4, 1, 3)
+plot(N4_50)
+
+N8_50 = tone_vocoder(x, Fs, 8, Fc);
+audiowrite('N8_50.wav', N8_50, Fs);
+subplot(4, 1, 4)
+plot(N8_50)
+
+% Task - 2
