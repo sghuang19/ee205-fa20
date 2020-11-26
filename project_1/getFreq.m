bd316f7be2b6baee8b45197d46ec1f0146b01b1f
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-27 00:19:39 +0800

    Revert "Format codes"
    This reverts commit 697a68cb129ceb20a3ebbbf0a39ca6df4cfa57c1.

--- project_1/getFreq.m
+++ project_1/getFreq.m
@@ -1,6 +1,7 @@
-function [low, high] = getFreq(N, i)
-    d200 = log10(200/165.4 + 1) / 0.06; d7000 = log10(7000/165.4 + 1) / 0.06;
-    delta = d7000 - d200;
-    dlow = d200 + (i - 1) / N * delta; dhigh = d200 + i * delta / N;
-    low = 165.4 * (10^(0.06 * dlow) - 1); high = 165.4 * (10^(0.06 * dhigh) - 1);
+function [low,high] = getFreq(N,i)
+d200=log10(200/165.4+1)/0.06;d7000=log10(7000/165.4+1)/0.06;
+delta=d7000-d200;
+dlow=d200+(i-1)/N*delta;dhigh=d200+i*delta/N;
+low=165.4*(10^(0.06*dlow)-1);high=165.4*(10^(0.06*dhigh)-1);
 end
+
