f4db538a231b897391d0ab9ee5c1058f97558cc2
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-27 00:43:04 +0800

    Task 2

--- project_1/tone_vocoder_test.m
+++ project_1/tone_vocoder_test.m
@@ -25,3 +25,24 @@ subplot(4, 1, 4)
 plot(N8_50)
 
 % Task - 2
+figure
+N = 4;
+N4_20 = tone_vocoder(x, Fs, N, 20);
+audiowrite('N4_20.wav', N4_20, Fs);
+subplot(4, 1, 1)
+plot(N1_50)
+
+N4_50 = tone_vocoder(x, Fs, N, 50);
+audiowrite('N4_50.wav', N4_50, Fs);
+subplot(4, 1, 2)
+plot(N2_50)
+
+N4_100 = tone_vocoder(x, Fs, N, 100);
+audiowrite('N4_100.wav', N4_100, Fs);
+subplot(4, 1, 3)
+plot(N4_50)
+
+N4_400 = tone_vocoder(x, Fs, N, 400);
+audiowrite('N4_400.wav', N4_400, Fs);
+subplot(4, 1, 4)
+plot(N8_50)
